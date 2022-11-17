489039ea084a94a90f7ecd9d81b3fbb71b87feb9
Yaroslav Yenkala <yaroslavrick@gmail.com>
2022-11-17 14:02:13 +0200

    added module Lib header to DataBase class

--- lib/database.rb
+++ lib/database.rb
@@ -1,12 +1,14 @@
-class DataBase
-  CURRENT_PATH = File.dirname(__FILE__)
-  DATABASE = 'db.yml'.freeze
+module Lib
+  class ::DataBase
+    CURRENT_PATH = File.dirname(__FILE__)
+    DATABASE = 'db.yml'.freeze
 
-  def initialize(db = DATABASE)
-    @db_name = db
-  end
+    def initialize(db = DATABASE)
+      @db_name = db
+    end
 
-  def load_database
-    YAML.safe_load(File.read("#{CURRENT_PATH}/db/#{@db_name}"))
+    def load_database
+      YAML.safe_load(File.read("#{CURRENT_PATH}/db/#{@db_name}"))
+    end
   end
 end
