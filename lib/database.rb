130cefddb331f84b8d2fa1a4694f466276f9a970
Yaroslav Yenkala <yaroslavrick@gmail.com>
2022-11-17 14:04:57 +0200

    added attr_reader to DataBase class

--- lib/database.rb
+++ lib/database.rb
@@ -3,12 +3,14 @@ module Lib
     CURRENT_PATH = File.dirname(__FILE__)
     DATABASE = 'db.yml'.freeze
 
+    attr_reader :db_name
+
     def initialize(db = DATABASE)
       @db_name = db
     end
 
     def load_database
-      YAML.safe_load(File.read("#{CURRENT_PATH}/db/#{@db_name}"))
+      YAML.safe_load(File.read("#{CURRENT_PATH}/db/#{db_name}"))
     end
   end
 end
