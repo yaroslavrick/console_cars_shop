3502d6db249613fc98c86422d6db19f11ee51fe7
Yaroslav Yenkala <yaroslavrick@gmail.com>
2022-11-22 23:49:02 +0200

    added attr_reader to search_engine_query

--- lib/search_engine_query.rb
+++ lib/search_engine_query.rb
@@ -1,5 +1,7 @@
 module Lib
   class SearchEngineQuery
+    attr_reader :data, :params
+
     def initialize(data:, params:)
       @data = data
       @params = params
@@ -13,41 +15,41 @@ module Lib
     private
 
     def search
-      search_by(@params[:make], 'make')
-      search_by(@params[:model], 'model')
-      search_by_range(@params[:price_from], @params[:price_to], 'price')
-      search_by_range(@params[:year_from], @params[:year_to], 'year')
+      search_by(params[:make], 'make')
+      search_by(params[:model], 'model')
+      search_by_range(params[:price_from], params[:price_to], 'price')
+      search_by_range(params[:year_from], params[:year_to], 'year')
     end
 
     def search_by(rule, option)
-      return @data if rule.empty?
+      return data if rule.empty?
 
-      @data.keep_if { |car| car[option].casecmp(rule).zero? }
+      data.keep_if { |car| car[option].casecmp(rule).zero? }
     end
 
     def search_by_range(from, to, rule)
-      return if @data.empty?
+      return if data.empty?
 
-      max_value = @data.max_by { |car| car[rule] }
+      max_value = data.max_by { |car| car[rule] }
       to = to.is_a?(Integer) ? to : max_value[rule]
-      @data.keep_if { |car| car[rule].between?(from.to_i, to.to_i) }
+      data.keep_if { |car| car[rule].between?(from.to_i, to.to_i) }
     end
 
     def sort_by_option(sort_option)
-      return @data.sort_by { |car| car['price'] } if sort_option.casecmp('price').zero?
+      return data.sort_by { |car| car['price'] } if sort_option.casecmp('price').zero?
 
-      @data.sort_by { |car| Date.strptime(car['date_added'], '%d/%m/%Y') }
+      data.sort_by { |car| Date.strptime(car['date_added'], '%d/%m/%Y') }
     end
 
     def sort_by_direction(sort_direction)
-      return @data if sort_direction.casecmp('asc').zero?
+      return data if sort_direction.casecmp('asc').zero?
 
-      @data.reverse
+      data.reverse
     end
 
     def sort
-      sort_by_option(@params[:sort_option])
-      sort_by_direction(@params[:sort_direction])
+      sort_by_option(params[:sort_option])
+      sort_by_direction(params[:sort_direction])
     end
   end
 end
