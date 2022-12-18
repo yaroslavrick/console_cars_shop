c2cb1ea2cdd38095e31ffec7ddfb2bce2393f4ff
Yaroslav Yenkala <yaroslavrick@gmail.com>
2022-12-18 23:53:12 +0200

    fixed car_db.rb

--- lib/tasks/car_db.rb
+++ lib/tasks/car_db.rb
@@ -1,16 +1,21 @@
-include Lib::Modules::Constants::ReadWriteType
-include Lib::Modules::Constants::FilePaths
+# frozen_string_literal: true
+
+WRITE = 'w'
+APPEND = 'a'
+CURRENT_PATH = File.dirname(__FILE__)
+PATH_TO_DB = File.join(CURRENT_PATH, '../db/db.yml').freeze
+PATH_TO_DB_BACKUP = File.join(CURRENT_PATH, '../db/db_backup.yml').freeze
 
 namespace :work_with_database do
   desc 'Clear whole database'
   task :clear_whole_database do
-    db = Lib::Models::DataBase.new(PATH_TO_DB)
+    db = Lib::Models::DataBase.new
     db.save(nil, WRITE, PATH_TO_DB)
   end
 
   desc 'Restore database'
   task :restore_database do
-    db = Lib::Models::DataBase.new(PATH_TO_DB_BACKUP)
+    db = Lib::Models::DataBase.new
     backup = db.load(PATH_TO_DB_BACKUP)
     db.save(backup, WRITE, PATH_TO_DB)
   end
@@ -19,14 +24,14 @@ end
 namespace :add_to_database do
   desc 'Add a car to database'
   task :add_car do
-    db = Lib::Models::DataBase.new(PATH_TO_DB)
+    db = Lib::Models::DataBase.new
     db.create_car(APPEND, PATH_TO_DB)
   end
 
   desc 'Add cars to database'
   task :add_cars do
-    cars = ENV.fetch('cars', nil).to_i
-    db = Lib::Models::DataBase.new(PATH_TO_DB)
+    cars = ENV.fetch('cars').to_i
+    db = Lib::Models::DataBase.new
     cars.times { db.create_car(APPEND, PATH_TO_DB) }
   end
 end
