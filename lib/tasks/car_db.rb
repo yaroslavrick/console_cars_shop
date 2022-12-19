8fbef1d860a1e89d140fd53f169c86946e8de027
Yaroslav Yenkala <yaroslavrick@gmail.com>
2022-12-19 11:04:29 +0200

    moved add rake tasks to one namespace, ranamed taksk names

--- lib/tasks/car_db.rb
+++ lib/tasks/car_db.rb
@@ -6,29 +6,27 @@ CURRENT_PATH = File.dirname(__FILE__)
 PATH_TO_DB = File.join(CURRENT_PATH, '../db/db.yml').freeze
 PATH_TO_DB_BACKUP = File.join(CURRENT_PATH, '../db/db_backup.yml').freeze
 
-namespace :work_with_database do
+namespace :database do
   desc 'Clear whole database'
-  task :clear_whole_database do
+  task :clear do
     db = Lib::Models::DataBase.new
     db.save(nil, WRITE, PATH_TO_DB)
   end
 
   desc 'Restore database'
-  task :restore_database do
+  task :restore do
     db = Lib::Models::DataBase.new
     backup = db.load(PATH_TO_DB_BACKUP)
     db.save(backup, WRITE, PATH_TO_DB)
   end
-end
 
-namespace :add_to_database do
   desc 'Add a car to database'
   task :add_car do
     db = Lib::Models::DataBase.new
     db.create_car(APPEND, PATH_TO_DB)
   end
 
-  desc 'Add cars to database'
+  desc 'Add cars to database (for example: rake database:add_cars cars=3)'
   task :add_cars do
     cars = ENV.fetch('cars').to_i
     db = Lib::Models::DataBase.new
